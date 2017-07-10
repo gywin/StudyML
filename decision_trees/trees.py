ac3f5259a2e58e0f177db42d5da6217804739460
gywin <https://github.com>
2017-07-10 21:57:11 +0800

    修改错误

--- decision_trees/trees.py
+++ decision_trees/trees.py
@@ -30,6 +30,12 @@ def calculate_entropy(dataset):
         prob = label_counts[key] / num
         entropy -= prob * np.log2(prob)
     return entropy
+	
+dataset, labels = creat_dataset()
+# print calculate_entropy(dataset)
+# dataset[0][-1] = 'maybe'
+# print calculate_entropy(dataset)
+
 
 def split_dataset(dataset, axis, value):
     rest_dataset = []
@@ -39,6 +45,8 @@ def split_dataset(dataset, axis, value):
             rest_vector.extend(feature_vector[axis+1:])
             rest_dataset.append(rest_vector)
     return rest_dataset
+	
+print split_dataset(dataset, 1, 1)
 
 def choose_best_feature(dataset):
     feature_num = len(dataset[0]) - 1
@@ -58,6 +66,9 @@ def choose_best_feature(dataset):
             best_feature = i
     return best_feature
 
+print choose_best_feature(dataset)
+	
+
 def majority_label(labels):
     labels_count = {}
     for vote in labels:
@@ -66,34 +77,54 @@ def majority_label(labels):
         labels_count[vote] += 1
     sorted_labels_count = sorted(labels_count.iteritems(), key=operator.itemgetter(1), reverse=True)
     return sorted_labels_count[0][0]
+	
+print majority_label(labels)
 
-def creat_tree(dataset, labels):
-    class_ls = [vector[-1] for vector in dataset]
-    if class_ls.count(class_ls[0]) == len(class_ls):
-        return class_ls[0]
-    if len(dataset) == 1:
-        return majority_label(class_ls)
-    best_feature = choose_best_feature(dataset)
-    best_feature_label =labels[best_feature]
-    my_tree = {best_feature:{}}
-    del(labels[best_feature])
-    feature_values = [vector[best_feature] for vector in dataset]
-    unique_values = set(feature_values)
-    for value in unique_values:
-        sub_labels = labels[:]
-        my_tree[best_feature_label][value] = creat_tree(split_dataset(dataset, best_feature, value), sub_labels)
-    return my_tree
+# def creat_tree(dataset, labels):
+    # class_ls = [vector[-1] for vector in dataset]
+    # if class_ls.count(class_ls[0]) == len(class_ls):
+        # return class_ls[0]
+    # if len(dataset[0]) == 1:
+        # return majority_label(class_ls)
+    # best_feature = choose_best_feature(dataset)
+    # best_feature_label =labels[best_feature]
+    # my_tree = {best_feature:{}}
+    # del(labels[best_feature])
+    # feature_values = [vector[best_feature] for vector in dataset]
+    # unique_values = set(feature_values)
+    # for value in unique_values:
+        # sub_labels = labels[:]
+        # my_tree[best_feature_label][value] = creat_tree(split_dataset(dataset, best_feature, value), sub_labels)
+    # return my_tree
 
-
-
-
+def create_tree(dataset, labels):
+    class_list = [vector[-1] for vector in dataset]
+    if class_list.count(class_list[0]) == len(class_list): 
+        return class_list[0]
+    if len(dataset[0]) == 1: 
+        return majority_label(class_list)
+    best_feat = choose_best_feature(dataset)
+    best_feat_label = labels[best_feat]
+    my_tree = {best_feat_label:{}}
+    del(labels[best_feat])
+    feat_values = [vector[best_feat] for vector in dataset]
+    unique_vals = set(feat_values)
+    for value in unique_vals:
+        sub_labels = labels[:]      
+        my_tree[best_feat_label][value] = create_tree(split_dataset(dataset, best_feat, value), sub_labels)
+    return my_tree    
+	
 if __name__ == '__main__':
     dataset, labels = creat_dataset()
-    creat_tree(dataset, labels)
+    # print createTree(dataset, labels)
+    print create_tree(dataset, labels)
+	
+	
+	
     # print dataset
-    # # print calculate_entropy(dataset)
-    # # dataset[0][-1] = 'maybe'
-    # # print dataset
-    # # print calculate_entropy(dataset)
-    # # print split_dataset(dataset, 0, 1)
+    # print calculate_entropy(dataset)
+    # dataset[0][-1] = 'maybe'
+    # print dataset
+    # print calculate_entropy(dataset)
+    # print split_dataset(dataset, 0, 1)
     # print choose_best_feature(dataset)
